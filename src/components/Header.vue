3ea44cbf2b36ff6806bc4731c83d6462b73bc746
benjiatch <benjaminchavez.sa@gmail.com>
2022-02-18 14:50:21 +0100

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<template>
+<div class="wrapper">
+    
+    <img src="@/assets/Sinus-Logo.svg"/>
+    <nav>
+    <router-link to="/"> Home </router-link>
+    <router-link to="/products"> Products </router-link>
+    <router-link to="/contact"> Contact </router-link>
+
+    <img class="icon" src="@/assets/Search-icon.svg" width="30"/>
+    <img class="icon" src="@/assets/Heart-icon.svg" width="30"/>
+    <img class="icon" src="@/assets/Profile-icon.svg" width="30"/>
+    <img class="icon" src="@/assets/Bag-icon.svg" width="30"/>
+
+
+
+
+    </nav>
+</div>
+    
+</template>
+
+
+<script>
+export default {
+    name: 'Header'
+}
+
+</script>
+
+<style lang="scss">
+
+.wrapper{
+display: flex;
+background:#333333;
+color: #FFFFFF;
+height: 160px;
+}
+
+img{
+    margin: 1rem;
+    display: flex;
+    justify-content: right;
+
+}
+
+
+nav{
+    display: flex;
+    justify-content: space-evenly;
+
+   a{ 
+    text-decoration: none;
+    margin: 1rem;
+    color: #FFFFFF;
+    font-size: 2rem;
+    font-weight: 100;
+    }
+
+   .icon{
+       margin: 1rem;
+       display: flex;
+   }
+}
+
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
+
+
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Products',
+    name: 'Products',
+    component: Products
+    
   }
 ]
 
