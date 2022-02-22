f2dd88290c6c5653a2eaae00826a5f55236b3a98
Qim Lönnegren <qim.lonnegren@iths.se>
2022-02-22 16:06:25 +0100

    Add basic structure to product

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,20 +1,33 @@
 <template>
   <div>
-      <Footer></Footer>
+    <Header />
+    <section>
+      <p>Hello</p>
+    </section>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Footer from '@/components/Footer.vue'
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-    components: {
-        Footer,
-    }
-
-}
+  name: "Product",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
+section {
+  background-color: grey;
+  width: 100%;
+  height: 400px;
+}
 </style>
