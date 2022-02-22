9e3a4837f68209fa91824ab6693ba3d5336c379b
David Öman <daoma90@gmail.com>
2022-02-22 22:14:24 +0100

    connect api for registration, login and user data

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,26 @@
 import axios from "axios";
 
-// axios.defaults.headers.common["Authorization"] = `Bearer`;
-// axios.defaults.baseURL = ""
+axios.defaults.baseURL = "http://localhost:5000/";
 
 // export async function AuthUser() {
 //   const response = await axios.post("/api/auth/");
 //   return response.data;
 // }
 
-export async function getUser() {
+export async function authUser(userCredentials) {
+  const credentials = userCredentials;
+  const response = await axios.post("/api/auth/", credentials);
+  return response;
+}
+
+export async function getUser(token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   const response = await axios.get("/api/me");
   return response;
 }
 
-export async function registerUser() {
-  const response = await axios.post("/api/register/");
+export async function registerUser(user) {
+  const body = user;
+  const response = await axios.post("/api/register/", body);
   return response;
 }
