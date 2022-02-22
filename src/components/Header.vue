7bd6e512fdf78709a39d5dcd5af1113f319258ce
benjiatch <benjaminchavez.sa@gmail.com>
2022-02-22 21:29:28 +0100

    Add fixed Header Nav bar and fixed small details

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,9 @@
     <img src="@/assets/Sinus-Logo.svg"/>
     <nav>
         <section class="nav-top">
-            <router-link to="/"> Home </router-link>
-            <router-link to="/products"> Products </router-link>
-            <router-link to="/contact"> Contact </router-link>
+            <router-link v-if="!isOpen" to="/"> Home </router-link>
+            <router-link v-if="!isOpen" to="/products"> Products </router-link>
+            <router-link v-if="!isOpen" to="/contact"> Contact </router-link>
 
             <img class="icon" src="@/assets/Search-icon.svg" @click="isOpen =!isOpen"/>
             <img class="icon" src="@/assets/Heart-icon.svg"/>
@@ -19,9 +19,6 @@
             <a>Attire</a>
             <a>Skateboards</a>
             <a>Sale</a>
-
-                        
-        
 
         </section>
 
@@ -60,10 +57,13 @@ export default {
 
 .wrapper{
 display: flex;
+justify-content: flex-end;
+flex-direction: row;
 background:#333333;
 color: #FFFFFF;
 height: 138px;
 width: 100%;
+
 }
 
 img{
@@ -74,28 +74,31 @@ img{
 
 
 nav{
+    display: flex;
+    flex-direction: column;
     width: 100%;
     height: 70px;
 
     .nav-top{
         display: flex;
-        justify-content: space-evenly;
+        justify-content: flex-end;
         }
     .nav-bottom{
         display: flex;
+        justify-content: flex-end;
         background-image: linear-gradient(to left, black, #333333);
-        
-    
+      
     }
+
 
     .nav-bottom a{
       
-        margin: 1rem 2.5rem;
+        margin: 1rem 4rem;
     }
 
    a{ 
     text-decoration: none;
-    margin: 1rem;
+    margin: 1rem 2rem;
     color: #FFFFFF;
     font-size: 2rem;
     font-weight: 100;
@@ -109,7 +112,7 @@ nav{
 
    .icon{
        width: 25px;
-       margin: 1rem;
+       margin: 1.4rem 1rem;
        display: flex;
    }
    
