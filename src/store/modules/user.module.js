29b65d82ef6df80679b12d0820fea2cde7496af6
David Öman <daoma90@gmail.com>
2022-02-22 22:16:02 +0100

    vuex connected to real api

--- src/store/modules/user.module.js
+++ src/store/modules/user.module.js
@@ -2,48 +2,62 @@
 import Actions from "@/store/action.types";
 import Mutations from "@/store/mutation.types";
 import Vue from "vue";
-import * as API from "../../api/index.mock";
+// import * as API from "../../api/index.mock";
+import * as API from "../../api/index";
 import store from "@/store";
 
 const state = () => ({
   token: null,
-  userData: {},
+  userData: {
+    name: "",
+    email: "",
+    role: "",
+    address: {
+      street: "",
+      city: "",
+      zip: null,
+    },
+  },
 });
 
 const getters = {};
 
 const actions = {
   async [Actions.LOGIN](context, credentials) {
-    const { status, data } = await API.authUser();
+    const { status, data } = await API.authUser(credentials);
     if (status === 200) {
-      context.commit(Mutations.SAVE_LOGIN, data);
-      context.commit(Mutations.CLOSE_LOGIN_REGISTRATION_MODAL);
+      await context.commit(Mutations.SAVE_LOGIN, data);
+      await API.getUser(data.token).then((response) => {
+        if (response.status === 200) {
+          context.commit(Mutations.SAVE_USER_DATA, response.data);
+          context.commit(Mutations.CLOSE_LOGIN_REGISTRATION_MODAL);
+        }
+      });
     }
   },
   [Actions.LOGOUT](context) {
     context.commit(Mutations.CLEAR_USER_DATA);
   },
-  async [Actions.REGISTER](context) {
-    const { status } = await API.registerUser();
+  async [Actions.REGISTER](context, register) {
+    const { status } = await API.registerUser(register);
     if (status === 200) {
+      context.commit(Mutations.REGISTER);
       return "success";
     }
-
-    context.commit(Mutations.REGISTER);
   },
 };
 
 const mutations = {
   async [Mutations.SAVE_LOGIN](state, token) {
-    state.token = token;
-    const { status, data } = await API.getUser();
+    state.token = token.token;
+  },
+  [Mutations.SAVE_USER_DATA](state, data) {
     state.userData = data;
   },
   [Mutations.CLEAR_USER_DATA](state) {
-    state.user = null;
+    state.userData = null;
     state.token = null;
   },
-  [Mutations.REGISTER](state) {},
 };
 
 export default {
