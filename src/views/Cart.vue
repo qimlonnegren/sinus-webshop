1425c88bb7703b9bf91ff0a07bb6922d7b3af5ff
StellaSFE96 <stella.samaranayake@iths.se>
2022-02-23 15:54:40 +0100

    cart style done

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,68 +1,96 @@
 <template>
-  <div>
+  <div class="background">
       <Header/>
-      <div class="content-main">
+      <div class="main-content">
           <h1 class="title">Varukorg</h1>
-          <div class="bottom-border"></div>
-          <div class="content">
-              <div class="left-section">
-                    <ul>
-                        <li>
-                            <div class="product-card">
+          <div class="bottom-border-one"></div>
+          <div class="grid-content">
+            <div class="product-list">
+                <ul>
+                    <li>
+                        <div class="product-card">
+                            <div class="left-span">
+                                <div class="product-img"></div>
+                            </div>
 
-                                <div class="left-span">
-                                    <div class="product-img"></div>
-                                </div>
+                            <div class="right-span">
+                                <h1>Product name</h1>
+                                <h2>Short description</h2>
+                                <p>Category</p>
+                                <h3>Price</h3>
 
-                                <div class="right-span">
-                                    <!-- <h1>{{product.title}}</h1>
-                                    <p>{{product.size}}</p>
-                                    <p>{{product.shortDesc}}</p>
-                                    <p>{{product.id}}</p>
-                                    <h2>{{product.price}}</h2> -->
-
-                                    <h1>title</h1>
-                                    <p>size</p>
-                                    <p>shortDesc</p>
-                                    <p>id</p>
-                                    <h2>price</h2>
-
-                                    <div class="right-span-bottom">
-                                        <img 
-                                        src="@/assets/trash.svg" 
-                                        alt="Trash can icon"
-                                        class="remove" 
-                                        @click="$emit('remove')"
-                                        >
-                                        <img 
-                                        class="icon" 
-                                        src="@/assets/Heart-icon-black.svg" 
-                                        width="30"
-                                        alt="Heart icon"
-                                        />    
-                                    </div>
-                                </div>
-                                <div class="container">
-                                    <div class="amount">
-                                        <select name="amount" id="amount">
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </select>
-                                    </div>
+                                <div class="right-span-bottom">
+                                    <img 
+                                    src="@/assets/trash.svg" 
+                                    alt="Trash can icon"
+                                    class="remove" 
+                                    @click="$emit('remove')"
+                                    >
+                                    <img 
+                                    class="icon" 
+                                    src="@/assets/Heart-icon-black.svg" 
+                                    width="30"
+                                    alt="Heart icon"
+                                    />    
                                 </div>
                             </div>
-                        </li>
-                    </ul>
-              </div>
-              <div class="right-section">
-                  <h1>summary box</h1>
-              </div>
+                            <div class="container">
+                                <div class="amount">
+                                    <select name="amount" id="amount">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="summary">
+                <div class="section-one">
+                    <h1>Discounts</h1>
+                    <h1>activate code</h1>
+                </div>
+
+                <div class="bottom-border-two"></div>
+
+                <div class="section-two">
+                    <h1>Order value</h1>
+                    <p>{{value}}</p>
+                </div>
+
+                <div class="section-three">
+                    <h1>Estimated delivery</h1>
+                    <p>{{delivery}}</p>
+                </div>
+
+                <div class="bottom-border-three"></div>
+
+                <div class="section-four">
+                    <h1>Sub total</h1>
+                    <p>{{total}}</p>
+                </div>
+                <div class="bottom-border-four"></div>
+
+                <div class="section-five">
+                    <routerlink to="/Checkout"><button>Checkout</button></routerlink>
+                    <p>Happy shopping!</p>
+                    <div class="icon-section">
+                        <img class="klarna" src="@/assets/checkout-icons/klarna-logo.svg" alt="klarna">
+                        <img class="paypal" src="@/assets/checkout-icons/paypal-logo.svg" alt="paypal">
+                        <img class="visa" src="@/assets/checkout-icons/Visa-logo.svg" alt="visa">
+                        <img class="mastercard" src="@/assets/checkout-icons/mastercard-logo.svg" alt="mastercard">
+                    </div>
+                </div>
+            </div>
           </div>
       </div>
+
       <Footer></Footer>
+
   </div>
 </template>
 
@@ -78,23 +106,26 @@ export default {
     Header,
     Footer
   },
-  props: {
-        products: Object,
-    },
-  data (){
-      return{
-          
+  data(){
+      return {
+          value: 200,
+          delivery: 59,
+          total: 259
       }
-  }
+  },
 }
 </script>
 
 
 <style lang="scss" scoped>
 
-.content {
-    display: flex;
-    justify-content: space-between;
+.background {
+    background-image: linear-gradient(rgb(243, 237, 238), rgb(209, 183, 186));
+}
+
+.main-content{
+    margin-left: 20px;
+    margin-right: 20px;
 }
 
 .flex-top {
@@ -108,36 +139,77 @@ export default {
     }
 }
 
-.product-card {
-    
+.grid-content{
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    grid-template-rows: repeat(4, 1fr);
+    grid-gap: 5px;
+}
+
+.product-list{
+    grid-row: 1 / 3;
+    grid-column: 1 / 5;
+}
+
+.summary {
+    background-color: #dbd9d9;
+    grid-row: 1 / 4;
+    grid-column: 6 / 10;
+}
+
+.product-card{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: auto;
 }
 
 .left-span {
-    background-color: #e7e7e7;
+    background-color: #ffffff;
     background-color: rgba(0,0,0,.08);
-    
+    grid-row: 1 / 3;
+    grid-column: 1 / 3;
 }
 
 .right-span {
+   
     text-align: left;
     padding-left: 10px;
+    margin-left: 10px;
     background-color: #e7e7e7;
     background-color: rgba(0,0,0,.04);
+    grid-row: 1 / 3;
+    grid-column: 3 / 5;
 }
 
 .container {
+    grid-row: 1 / 3;
+    grid-column: 5 / 6;
 }
 
-.amount{
-    width: 80px;
-    height: 50px;
+.amount {
     background-color: #e7e7e7;
     background-color: rgba(0,0,0,.04);
+    width: 60px;
+    height: 60px;
+    margin-top: 290px;
+}
+
+select {
+    border: none;
+    width: 50px;
+    height: 40px;
+    margin-top: 10px;
+    background-color: #b8b7b7;
+    cursor: pointer;
+    padding: 5px;
+    font-family: 'Mulish', sans-serif;
 }
 
 .right-span-bottom {
     display: flex;
     justify-content: space-between;
+    padding-right: 10px;
+    padding-top: 125px;   
 }
 
 .title {
@@ -146,8 +218,105 @@ export default {
     font-family: 'Quicksand', sans-serif;
 }
 
-.bottom-border {
-    border-bottom: 2px solid rgb(201, 199, 199);
+.bottom-border-one {
+    border-bottom: 2px solid rgb(182, 182, 182);
+    margin-bottom: 20px;
+}
+
+.bottom-border-two {
+    border-bottom: 2px solid rgb(175, 174, 174);
+    margin: 10px 30px 10px 30px;
+    border-radius: 3px;
+}
+
+.bottom-border-three {
+    border-bottom: 4px solid rgb(155, 153, 153);
+    margin: 20px 30px;
+    border-radius: 5px;
+}
+
+.bottom-border-four {
+    border-bottom: 2px solid rgb(219, 216, 216);
+    margin: 20px 70px;
+    border-radius: 5px;
+}
+
+.section-one {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    padding-right: 30px;
+    margin-top: 20px;
+}
+
+.section-two {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    padding-right: 30px;
+}
+
+.section-three {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    padding-right: 30px;
+}
+
+.section-four {
+    display: flex;
+    justify-content: space-between;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    p{
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+}
+
+.section-five{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .klarna{
+        width: 40px;
+    }
+    .paypal{
+        width: 30px;
+    }
+    .visa{
+        width: 55px;
+    }
+    .mastercard{
+        width: 45px;
+    }
+
+    p {
+        padding-bottom: 30px;
+    }
+
+    button {
+        background-image: linear-gradient(rgb(147, 228, 167), rgb(39, 126, 78));
+        color: white;
+        font-family: 'Quicksand', sans-serif;
+        font-size: 1.8rem;
+        border: none;
+        border-radius: 50px;
+        width: 240px;
+        height: 50px;
+        margin: 30px;
+        cursor: pointer;
+    }
+} 
+
+.icon-section{
+    width: 400px;
+    margin-bottom: 20px;
+    display: flex;
+    justify-content: space-around;
 }
 
 li {
@@ -156,16 +325,33 @@ li {
 
 ul {
     padding: 0px;
+    margin: 0px;
+}
+
+h1 {
+    font-size: 1.5rem;
+}
+
+h2 {
+    font-size: 1rem;
+    font-weight: 400;
 }
 
 p,
 h1,
-h2 {
+h2,
+h3 {
     font-family: 'Mulish', sans-serif;
 }
 
 .remove {
     width: 20px;
+    cursor: pointer;
+}
+
+.icon{
+    cursor: pointer;
+    margin-right: 10px;
 }
 
 </style>
