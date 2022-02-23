58d41f9284d89f3487c28beda291a30ae4f7acd3
Qim Lönnegren <qim.lonnegren@iths.se>
2022-02-23 21:11:12 +0100

    Add styling for Product view

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,7 +2,21 @@
   <div>
     <Header />
     <section>
-      <p>Hello</p>
+      <img src="../assets/Heart-icon.svg" alt="Product image" />
+      <i v-if="product.favorite" :key="product.favorite" title="Added to wishlist!" class="fa-solid fa-heart"></i>
+      <i v-else title="Click to add to wishlist" class="fa-regular fa-heart"></i>      
+      <i v-if="product.specialEdition" :key="product.specialEdition" class="fa-solid fa-star" title="Special edition item!"></i>
+      <i v-else :key="product.specialEdition" class="fa-regular fa-star" title="Not special edition"></i>
+      <span>
+        <h1>{{product.title}}</h1>
+        <p>{{product.longDesc}}</p>
+        <p>{{product.price}} £</p>
+        <button>Choose Size</button>
+        <button>Buy</button>
+      </span>
+    </section>
+    <section>
+        <h2>You might also like...</h2>
     </section>
     <Footer></Footer>
   </div>
@@ -18,19 +32,93 @@ export default {
     Header,
     Footer,
   },
-  data() {
-    return {};
+  data() { return {
+      product:
+        {
+          title: "TITLE",
+          specialEdition: "False",
+          price: "123",
+          category: "Skateboards",
+          longDesc:
+            "Smith grind bail fastplant nose-bump Chris Haslam hard flip nose grab.",
+          imgFile: "sinus-cap-blue.png",
+          favorite: "true",
+        },
+    };
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
+@import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css");
+
 section {
-  background-color: grey;
-  width: 100%;
-  height: 200px;
+  width: 75%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: auto auto 5rem auto;
+  color: black;
+  background-color: rgb(199, 229, 255);
+  margin: 3rem auto 3rem auto;
+  position: relative;
 }
-p{
-    color: pink;
+img{
+  width: 20rem;
+  height: 21rem;
+  background-color: #b45a5a;
+}
+span{
+    text-align: left;
+}
+h1{
+    width: 22rem;
+    margin: 0;
+    top: 0;
+}
+p {
+  color: pink;
+  width: 22rem;
+}
+.fa-heart{
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  font-size: x-large;
+}
+.fa-solid{
+  color: #B84040;
+}
+.fa-star{
+  color: rgb(245, 245, 19);
+  font-size: x-large;
+  position: absolute;
+  top: 15px;
+  left: 15px;
+}
+button{
+  width: 10rem;
+  height: 2.5rem;
+  font-size: large;
+  border: 1px solid #b45a5a;
+  border-radius: 20px;
+  font-family: 'Mulish', sans-serif;
+  font-weight: bold;
+  color: white;
+  background: linear-gradient(#e78b8b, #b35a5a);
+  letter-spacing: 2px;
+  margin: 0.8rem 1rem 0 1rem;
+  box-shadow: 0px 4px 4px #888888;
+  cursor: pointer;
+	&:hover{
+		background: linear-gradient(#d37171, #7a3333);
+	}
+	&:active{
+		background: linear-gradient(#e78b8b, #b35a5a);
+	}
 }
 </style>
