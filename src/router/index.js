47bd2f26ea516f443cd137986cd954f50501b9e7
StellaSFE96 <stella.samaranayake@iths.se>
2022-02-24 09:46:53 +0100

    minor changes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
+import Wishlist from '../components/Wishlist.vue'
 import Cart from '../views/Cart.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
@@ -19,9 +21,19 @@ const routes = [
 
   },
   {
+    path: '/Wishlist',
+    name: 'Wishlist',
+    component: Wishlist
+  },
+  {
     path: '/Cart',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/Checkout',
+    name: 'Checkout',
+    component: Checkout
   }
 ]
 
