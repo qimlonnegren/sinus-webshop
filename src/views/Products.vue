241596b5f513c819ccc15d5d9a9d0b43f94b26a4
Qim Lönnegren <qim.lonnegren@iths.se>
2022-02-24 13:27:14 +0100

    Remove wrapper class

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -52,11 +52,6 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Mulish&display=swap');
 @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css");
 
-.wrapper {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
 section {
   width: 70%;
   height: auto;
