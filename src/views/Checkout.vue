ee7e1f3e040fcfda9115b3882d06918d12a95cd0
David Öman <daoma90@gmail.com>
2022-02-28 21:16:39 +0100

    Add Checkout page Add Profile page

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,14 +1,397 @@
 <template>
-  <div></div>
+  <div>
+    <Header />
+    <div class="checkoutContainer">
+      <div class="checkoutSection">
+        <div class="checkoutInputContainer">
+          <h2 class="header">Delivery address</h2>
+          <div class="inputContainer">
+            <input type="text" placeholder="Address" v-model="customerInfo.address" />
+            <input type="text" placeholder="Address" />
+            <div class="inputRow">
+              <input type="text" placeholder="Zip" v-model="customerInfo.zip" />
+              <input type="text" placeholder="City" v-model="customerInfo.city" />
+            </div>
+          </div>
+        </div>
+        <div class="checkoutInputContainer">
+          <h2 class="header">Shipping</h2>
+          <div class="inputContainer">
+            <div class="shippingContainer">
+              <div
+                class="shippingOption"
+                @click="selectShipping('postmord', 0)"
+                v-bind:class="{ selected: this.shipping.company === 'postmord' }"
+              >
+                <p>PostMord</p>
+                <p>FREE</p>
+              </div>
+              <div
+                class="shippingOption"
+                @click="selectShipping('bring', 69)"
+                v-bind:class="{ selected: this.shipping.company === 'bring' }"
+              >
+                <p>Bring</p>
+                <p>69 SEK</p>
+              </div>
+              <div
+                class="shippingOption"
+                @click="selectShipping('instabox', 49)"
+                v-bind:class="{ selected: this.shipping.company === 'instabox' }"
+              >
+                <p>Instabox</p>
+                <p>49 SEK</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="checkoutInputContainer">
+          <h2 class="header">Payment</h2>
+          <div class="paymentContainer">
+            <img
+              class="paymentIcon"
+              src="@/assets/visa.svg"
+              @click="selectCardbrand('visa')"
+              v-bind:class="{ selected: this.cardBrand === 'visa' }"
+            />
+            <img
+              class="paymentIcon"
+              src="@/assets/mastercard.svg"
+              @click="selectCardbrand('mastercard')"
+              v-bind:class="{ selected: this.cardBrand === 'mastercard' }"
+            />
+          </div>
+          <div class="inputContainer">
+            <input type="text" placeholder="Card" />
+            <div class="inputRow">
+              <input type="text" placeholder="ccv" />
+              <input type="text" placeholder="Date" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="checkoutSection">
+        <div class="cartContainer">
+          <h2 class="header">Cart</h2>
+          <ul class="cartList">
+            <li v-for="product in products" :key="product.title" class="product">
+              <img class="productImg" src="@/assets/Hero.svg" />
+              <div class="productInfo">
+                <p>{{ product.title }}</p>
+                <p>{{ product.price }} SEK</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="orderContainer">
+          <div class="checkoutRow">
+            <p>Order value</p>
+            <p>{{ calculateOrderValue }} SEK</p>
+          </div>
+          <div class="checkoutRow bottomBorder">
+            <p>Delivery</p>
+            <p>{{ shippingPrice }}</p>
+          </div>
+          <div class="checkoutRow">
+            <p>Total</p>
+            <p>{{ calculateTotalPrice }} SEK</p>
+          </div>
+          <div class="buttonContainer">
+            <button @click="handlePostOrder" class="button greenButton">Complete Order</button>
+            <button class="button greyButton">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
+import * as API from "../api/index";
 export default {
-    name: 'Checkout',
-
-}
+  name: "Checkout",
+  components: {
+    Footer,
+    Header,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 40,
+          title: "Blue",
+          specialEdition: false,
+          price: 149,
+          category: "cap",
+          shortDesc: "unisex",
+          longDesc: "Smith grind bail fastplant nose-bump Chris Haslam hard flip nose grab.",
+          imgFile: "sinus-cap-blue.png",
+          createdAt: "2022-02-21T13:13:53.017Z",
+          updatedAt: "2022-02-21T13:13:53.017Z",
+        },
+        {
+          id: 41,
+          title: "Blue",
+          specialEdition: false,
+          price: 149,
+          category: "cap",
+          shortDesc: "unisex",
+          longDesc: "Smith grind bail fastplant nose-bump Chris Haslam hard flip nose grab.",
+          imgFile: "sinus-cap-blue.png",
+          createdAt: "2022-02-21T13:13:53.017Z",
+          updatedAt: "2022-02-21T13:13:53.017Z",
+        },
+        {
+          id: 43,
+          title: "Blue",
+          specialEdition: false,
+          price: 149,
+          category: "cap",
+          shortDesc: "unisex",
+          longDesc: "Smith grind bail fastplant nose-bump Chris Haslam hard flip nose grab.",
+          imgFile: "sinus-cap-blue.png",
+          createdAt: "2022-02-21T13:13:53.017Z",
+          updatedAt: "2022-02-21T13:13:53.017Z",
+        },
+      ],
+      customerInfo: {
+        address: "",
+        address2: "",
+        zip: "",
+        city: "",
+      },
+      cardDetails: {
+        cardNumber: "",
+        cardCCV: "",
+        cardDate: "",
+      },
+      shipping: {
+        company: "",
+        price: "",
+      },
+      cardBrand: "",
+    };
+  },
+  methods: {
+    populateAddressInputs() {
+      if (this.$store.state.userModule.token !== null) {
+        this.customerInfo.address = this.$store.state.userModule.userData.address.street;
+        this.customerInfo.zip = this.$store.state.userModule.userData.address.zip;
+        this.customerInfo.city = this.$store.state.userModule.userData.address.city;
+      }
+    },
+    selectShipping(shippingOption, shippingPrice) {
+      this.shipping.company = shippingOption;
+      this.shipping.price = shippingPrice;
+    },
+    selectCardbrand(cardBrand) {
+      this.cardBrand = cardBrand;
+    },
+    async handlePostOrder() {
+      const items = this.products.map((product) => product.id);
+      console.log("items", items);
+      const response = await API.postOrder(items, this.$store.state.userModule.token);
+      console.log("response", response);
+    },
+  },
+  computed: {
+    calculateOrderValue() {
+      let price = 0;
+      this.products.forEach((product) => {
+        price += product.price;
+      });
+      return price;
+    },
+    shippingPrice() {
+      if (this.shipping.company === "") {
+        return "";
+      } else {
+        return this.shipping.company === "postmord" ? "FREE" : this.shipping.price + " SEK";
+      }
+    },
+    calculateTotalPrice() {
+      let price = 0;
+      this.products.forEach((product) => {
+        price += product.price;
+      });
+      if (this.shipping.company !== "postmord") {
+        price += this.shipping.price;
+      }
+      return price;
+    },
+  },
+  mounted() {
+    this.populateAddressInputs();
+  },
+};
 </script>
 
 <style>
+.checkoutContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 50px;
+}
 
+.checkoutSection {
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  min-width: 500px;
+  height: 100vh;
+  /* background-color: pink; */
+  margin-top: 20px;
+}
+
+.checkoutInputContainer {
+  display: flex;
+  flex-direction: column;
+}
+
+.header {
+  font-size: 30px;
+  margin-bottom: 10px;
+}
+
+.inputContainer {
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  gap: 10px;
+}
+
+.inputRow {
+  display: flex;
+  flex-direction: row;
+  width: 350px;
+  gap: 10px;
+}
+
+input {
+  height: 50px;
+  width: 100%;
+}
+
+.shippingContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+}
+
+.shippingOption {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  padding: 10px;
+  border-radius: 15px;
+  border: 3px solid transparent;
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.paymentContainer {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.paymentIcon {
+  height: 100px;
+  object-fit: contain;
+  background-color: white;
+  padding: 10px 20px 10px 20px;
+  border-radius: 15px;
+  margin: 0;
+  border: 3px solid transparent;
+  cursor: pointer;
+}
+
+.cartList {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0;
+}
+
+.product {
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
+  padding-bottom: 10px;
+}
+
+.product:not(:last-of-type) {
+  border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+}
+
+.productImg {
+  height: 100px;
+  width: 100px;
+  margin: 0;
+  background-color: white;
+  object-fit: cover;
+}
+
+.productInfo {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.orderContainer {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.checkoutRow {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.bottomBorder {
+  border-bottom: 2px solid rgba(0, 0, 0, 0.3);
+  padding-bottom: 10px;
+}
+
+.buttonContainer {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.button {
+  width: 200px;
+  height: 50px;
+  border-radius: 32px;
+  border: none;
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  font-size: 20px;
+}
+
+.greenButton {
+  background: linear-gradient(180deg, rgba(255, 255, 255, 0.4) 0%, rgba(0, 0, 0, 0.1) 100%), #0fa81f;
+  color: white;
+}
+
+.greyButton {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.3045) 0%, rgba(255, 255, 255, 0) 100%), #dbdbdb;
+  color: black;
+}
+
+.selected {
+  border: 3px solid green;
+}
 </style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,161 @@
+<template>
+  <div>
+    <Header />
+    <section class="profileContainer">
+      <article class="profileSection">
+        <h2 class="header">Account info</h2>
+        <div class="accountInfoContainer">
+          <p class="accountInfo">{{ profileData.name }}</p>
+          <p class="accountInfo">{{ profileData.email }}</p>
+          <p class="accountInfo">{{ profileData.address.street }}</p>
+          <div class="accountInfoRow">
+            <p class="accountInfo">{{ profileData.address.zip }}</p>
+            <p class="accountInfo">{{ profileData.address.city }}</p>
+          </div>
+        </div>
+      </article>
+      <article class="profileSection">
+        <h2 class="header">Orders</h2>
+        <ul class="orderList">
+          <li class="orderItem" v-for="item in orders" :key="item.id">
+            <p class="orderInfo">ID: {{ item.id }}</p>
+            <p class="orderInfo">Price: {{ calculateTotalOrderPrice(item.items) }}</p>
+            <p class="orderInfo">Status: {{ getOrderStatus(item.status) }}</p>
+          </li>
+        </ul>
+      </article>
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "@/components/Footer.vue";
+import Header from "@/components/Header.vue";
+import * as API from "../api/index";
+
+export default {
+  components: {
+    Footer,
+    Header,
+  },
+  data() {
+    return {
+      profileData: this.$store.state.userModule.userData,
+      orders: [
+        {
+          id: 123,
+          status: "inProcess",
+          shippingCity: "El stockholmo",
+          shippingStreet: "Street 1",
+          shippingZip: "12345",
+          createdAt: "2022-02-16 13:13:28.138 +00:00",
+          updatedAt: "2022-02-16 13:13:28.138 +00:00",
+          UserId: 1337,
+          items: [
+            { ProductId: 5, price: 899 },
+            { ProductId: 6, price: 1999 },
+          ],
+        },
+        {
+          id: 123,
+          status: "inProcess",
+          shippingCity: "El stockholmo",
+          shippingStreet: "Street 1",
+          shippingZip: "12345",
+          createdAt: "2022-02-16 13:13:28.138 +00:00",
+          updatedAt: "2022-02-16 13:13:28.138 +00:00",
+          UserId: 1337,
+          items: [
+            { ProductId: 5, price: 899 },
+            { ProductId: 6, price: 1999 },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    calculateTotalOrderPrice(products) {
+      let totalPrice = 0;
+      products.map((product) => {
+        totalPrice += product.price;
+      });
+      return totalPrice;
+    },
+    getOrderStatus(status) {
+      switch (status) {
+        case "inProcess":
+          return "In process";
+        case "canceled":
+          return "Cancelled";
+        case "shipped":
+          return "Shipped";
+      }
+    },
+  },
+  computed: {},
+  created: async function () {
+    const { data } = await API.fetchOrders(this.$store.state.userModule.token);
+    this.orders = data;
+  },
+};
+</script>
+
+<style>
+.profileContainer {
+  min-height: 80vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.profileSection {
+  display: flex;
+  flex-direction: column;
+  width: 500px;
+  /* background-color: pink; */
+  margin-top: 5vh;
+}
+
+.header {
+  font-size: 36px;
+  width: max-content;
+}
+
+.accountInfoContainer {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  margin-top: 20px;
+}
+
+.accountInfo {
+  font-size: 25px;
+}
+
+.accountInfoRow {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+}
+
+.orderList {
+  padding: 0;
+  margin-top: 10px;
+}
+
+.orderItem {
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  padding-bottom: 10px;
+  padding-top: 10px;
+}
+
+.orderInfo {
+  font-size: 20px;
+}
+</style>
