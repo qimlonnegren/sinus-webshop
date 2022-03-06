306bdb799cddd673124b0bcb1f5c8d43ac2a12d5
Qim Lönnegren <qim.lonnegren@iths.se>
2022-03-06 20:13:40 +0100

    Add pagination functionality for categories

--- src/store/index.js
+++ src/store/index.js
@@ -9,155 +9,155 @@ import * as API from "../api/index";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    loginRegistrationModalVisible: false,
-    items: [],
-    products: {},
-    cart: [],
-    wishlist: [],
-    attire: [],
-    skateboards: [],
-    accessories: [],
-    singleProduct: [],
+    state: {
+        loginRegistrationModalVisible: false,
+        items: [],
+        products: {},
+        cart: [],
+        wishlist: [],
+        attire: [],
+        skateboards: [],
+        accessories: [],
+        singleProduct: [],
 
-  },
-  mutations: {
-    [Mutations.OPEN_LOGIN_REGISTRATION_MODAL](state) {
-      state.loginRegistrationModalVisible = true;
     },
-    [Mutations.CLOSE_LOGIN_REGISTRATION_MODAL](state) {
-      state.loginRegistrationModalVisible = false;
-    },
-    // here
-    saveItems(state, products) {
-      state.items = products;
-      for (let product of products) {
-        Vue.set(state.products, product.id, product);
-      }
-    },
-    saveSingleProduct(state, data){
-        state.singleProduct = data;
-        Vue.set(state.products, product.id, product)
-    },
-    saveProductsInCart(state, product) {
-      const inCart = state.cart.find((cartItem) => cartItem.id == product.id);
-      if (inCart) {
-        inCart.amount++;
-      } else {
-        state.cart.push({
-          id: product.id,
-          amount: 1,
-        });
-      }
-    },
-    updateCart(state, { id, amount }) {
-      const inCart = state.cart.find((cartItem) => cartItem.id == id);
-      inCart.amount = amount;
-    },
-    removeProduct(state, product) {
-      state.cart.splice(state.cart.indexOf(product), 1);
-    },
-    incItemButton(state, product) {
-      state.cart[state.cart.indexOf(product)].amount++;
-    },
-    decItemButton(state, product) {
-      state.cart[state.cart.indexOf(product)].amount--;
-    },
-    addToWishlist(state, product) {
-      if (!state.wishlist.includes(product)) {
-        state.wishlist.push(product);
-      }
-    },
-    saveAttireCategory(state, res){
-        state.attire = res;
-    },
-    saveSkateboardCategory(state, res){
-        state.skateboards = res;
-    },
-    saveAccessoriesCategory(state, res){
-        state.accessories = res;
-    },
-    // saveMoreAttire(state, res){
-    //     res.forEach((product) => {
-    //         state.attire.push(product);
-    //     });
-    // },
-    
-  },
+    mutations: {
+        [Mutations.OPEN_LOGIN_REGISTRATION_MODAL](state) {
+            state.loginRegistrationModalVisible = true;
+        },
+        [Mutations.CLOSE_LOGIN_REGISTRATION_MODAL](state) {
+            state.loginRegistrationModalVisible = false;
+        },
+        // here
+        saveItems(state, products) {
+            state.items = products;
+            for (let product of products) {
+                Vue.set(state.products, product.id, product);
+            }
+        },
+        saveSingleProduct(state, data) {
+            state.singleProduct = data;
+            Vue.set(state.products, product.id, product)
+        },
+        saveProductsInCart(state, product) {
+            const inCart = state.cart.find((cartItem) => cartItem.id == product.id);
+            if (inCart) {
+                inCart.amount++;
+            } else {
+                state.cart.push({
+                    id: product.id,
+                    amount: 1,
+                });
+            }
+        },
+        updateCart(state, { id, amount }) {
+            const inCart = state.cart.find((cartItem) => cartItem.id == id);
+            inCart.amount = amount;
+        },
+        removeProduct(state, product) {
+            state.cart.splice(state.cart.indexOf(product), 1);
+        },
+        incItemButton(state, product) {
+            state.cart[state.cart.indexOf(product)].amount++;
+        },
+        decItemButton(state, product) {
+            state.cart[state.cart.indexOf(product)].amount--;
+        },
+        addToWishlist(state, product) {
+            if (!state.wishlist.includes(product)) {
+                state.wishlist.push(product);
+            }
+        },
+        saveAttireCategory(state, response) {
+            state.attire = response;
+        },
+        saveSkateboardCategory(state, response) {
+            state.skateboards = response;
+        },
+        saveAccessoriesCategory(state, response) {
+            state.accessories = response;
+        },
+        // saveMoreAttire(state, res){
+        //     res.forEach((product) => {
+        //         state.attire.push(product);
+        //     });
+        // },
 
-  actions: {
-    [Actions.OPEN_LOGIN_REGISTRATION_MODAL](context) {
-      context.commit(Mutations.OPEN_LOGIN_REGISTRATION_MODAL);
-    },
-    [Actions.CLOSE_LOGIN_REGISTRATION_MODAL](context) {
-      context.commit(Mutations.CLOSE_LOGIN_REGISTRATION_MODAL);
-    },
-    async fetchItems(context, paylode) {
-      const response = await API.getItems(paylode.page);
-      context.commit("saveItems", response.data);
-    },
-    async getSingleProduct(context, id){
-        const res = await API.getSingleProduct(id)
-        context.commit("saveSingleProduct", res.data.post)
-    },
-    async getAttireCategory(context){
-        const res = await API.getAttireCategory();
-        context.commit("saveAttireCategory", res.data);
-    },
-    async getSkateboardsCategory(context){
-        const res = await API.getSkateboardsCategory();
-        context.commit("saveSkateboardCategory", res.data);
-    },
-    async getAccessoriesCategory(context){
-        const res = await API.getAccessoriesCategory();
-        context.commit("saveAccessoriesCategory", res.data);
-    },
-    // async getMoreAttire(context){
-    //     const res = await API.getMoreAttire(context.state.page);
-    //     if (context.state.page <= 3){
-    //         context.commit("getMore");
-    //         context.commit("saveMoreAttire", res.data)
-    //     }
-    // },
-
-    addToCart({ commit }, product) {
-      commit("saveProductsInCart", product);
-    },
-    updateCartAmount({ commit }, { id, amount }) {
-      commit("updateCart", { id, amount });
-    },
-    incItemButton(context, product) {
-      context.commit("incItemButton", product);
-    },
-    decItemButton(context, product) {
-      context.commit("decItemButton", product);
-    },
-    removeCartProduct({ commit }, product) {
-      commit("removeProduct", product);
-    },
-    addToWishlist({ commit }, product) {
-      commit("addToWishlist", product);
     },
 
-  },
-  getters: {
-    cartTotal(state) {
-        return state.cart.reduce((total, product) => {
-          return total + product.amount * state.products[product.id].price;
-        }, 0);
-      },
-    cart(state) {
-      return state.cart.map((product) => ({
-        id: product.id,
-        title: state.products[product.id].title,
-        category: state.products[product.id].category,
-        imgFile: state.products[product.id].imgFile,
-        amount: product.amount,
-        price: state.products[product.id].price,
-      }));
+    actions: {
+        [Actions.OPEN_LOGIN_REGISTRATION_MODAL](context) {
+            context.commit(Mutations.OPEN_LOGIN_REGISTRATION_MODAL);
+        },
+        [Actions.CLOSE_LOGIN_REGISTRATION_MODAL](context) {
+            context.commit(Mutations.CLOSE_LOGIN_REGISTRATION_MODAL);
+        },
+        async fetchItems(context, payload) {
+            const response = await API.getItems(payload.page);
+            context.commit("saveItems", response.data);
+        },
+        async getSingleProduct(context, id) {
+            const res = await API.getSingleProduct(id)
+            context.commit("saveSingleProduct", res.data.post)
+        },
+        async getAttireCategory(context, payload) {
+            const response = await API.getAttireCategory(payload.page);
+            context.commit("saveAttireCategory", response.data);
+        },
+        async getSkateboardsCategory(context, payload) {
+            const response = await API.getSkateboardsCategory(payload.page);
+            context.commit("saveSkateboardCategory", response.data);
+        },
+        async getAccessoriesCategory(context, payload) {
+            const response = await API.getAccessoriesCategory(payload.page);
+            context.commit("saveAccessoriesCategory", response.data);
+        },
+        // async getMoreAttire(context){
+        //     const res = await API.getMoreAttire(context.state.page);
+        //     if (context.state.page <= 3){
+        //         context.commit("getMore");
+        //         context.commit("saveMoreAttire", res.data)
+        //     }
+        // },
+
+        addToCart({ commit }, product) {
+            commit("saveProductsInCart", product);
+        },
+        updateCartAmount({ commit }, { id, amount }) {
+            commit("updateCart", { id, amount });
+        },
+        incItemButton(context, product) {
+            context.commit("incItemButton", product);
+        },
+        decItemButton(context, product) {
+            context.commit("decItemButton", product);
+        },
+        removeCartProduct({ commit }, product) {
+            commit("removeProduct", product);
+        },
+        addToWishlist({ commit }, product) {
+            commit("addToWishlist", product);
+        },
+
     },
-  },
-  modules: {
-    userModule: UserModule,
-  },
+    getters: {
+        cartTotal(state) {
+            return state.cart.reduce((total, product) => {
+                return total + product.amount * state.products[product.id].price;
+            }, 0);
+        },
+        cart(state) {
+            return state.cart.map((product) => ({
+                id: product.id,
+                title: state.products[product.id].title,
+                category: state.products[product.id].category,
+                imgFile: state.products[product.id].imgFile,
+                amount: product.amount,
+                price: state.products[product.id].price,
+            }));
+        },
+    },
+    modules: {
+        userModule: UserModule,
+    },
 });
--- src/views/Accessories.vue
+++ src/views/Accessories.vue
@@ -6,6 +6,11 @@
         <SingleProduct :product="product" />
       </div>
     </div>
+    <span>
+      <i @click="previousPage()" class="fa-solid fa-angle-left"></i>
+      <p class="page-text">{{ currentPage }}/{{ maxPage }}</p>
+      <i @click="nextPage()" class="fa-solid fa-angle-right"></i>
+    </span>
   </div>
 </template>
 
@@ -16,13 +21,38 @@ export default {
   components: {
     SingleProduct,
   },
+  created() {
+    this.$store.dispatch("getAccessoriesCategory", { page: this.currentPage });
+  },
   computed: {
     products() {
       return this.$store.state.accessories;
     },
   },
-  created() {
-    this.$store.dispatch("getAccessoriesCategory");
+  data() {
+    return {
+      currentPage: 1,
+      minPage: 1,
+      maxPage: 2,
+    };
+  },
+  methods: {
+    previousPage() {
+      if (this.currentPage > this.minPage) {
+        this.currentPage--;
+        this.$store.dispatch("getAccessoriesCategory", {
+          page: this.currentPage,
+        });
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.maxPage) {
+        this.currentPage++;
+        this.$store.dispatch("getAccessoriesCategory", {
+          page: this.currentPage,
+        });
+      }
+    },
   },
 };
 </script>
@@ -49,5 +79,30 @@ export default {
   h1 {
     margin: 20px;
   }
+}
+.fa-circle-arrow-left,
+.fa-circle-arrow-right {
+  font-size: xx-large;
+}
+.fa-angle-left,
+.fa-angle-right {
+  color: #d37171;
+  font-size: 50px;
+  &:hover {
+    color: #7a3333;
+  }
+  &:active {
+    color: #ffb0b0;
+  }
+}
+span {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+}
+span p {
+  font-size: x-large;
+  padding: 1rem;
 }
 </style>
